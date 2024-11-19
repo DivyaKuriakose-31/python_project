888bcf2f99f4c9878c8daef3ea5c8cd2f535d53c
DivyaKuriakose-31 <divyakuriakose2028@cs.sjcetpalai.ac.in>
2024-11-19 15:54:09 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,28 @@
+#increasing triangle
+limit=int(input("Enter a limit"))
+for i in range(limit):
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
+#decreasing triangle
+limit=int(input("Enter a limit:"))
+for i in range(limit):
+    for j in range(limit-i):
+        print("*",end=" ")
+    print()
+#hill triangle
+limit=int(input("Enter the limit:"))
+for i in range(1,limit+1):
+    for j in range(limit-i):
+        print(" ",end="")
+    for k in range(2*i-1):
+            print("*",end="")
+    print()
+#reverse hill triangle
+limit=int(input("Enter the limit:"))
+for i in range(limit,0,-1):
+    for j in range(limit-i):
+        print(" ",end="")
+    for k in range(1,2*i):
+        print("*",end="")
+    print()
