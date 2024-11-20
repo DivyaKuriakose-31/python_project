acd1630db1b4635d278c635a6d7b3431070e5d44
DivyaKuriakose-31 <divyakuriakose2028@cs.sjcetpalai.ac.in>
2024-11-20 23:14:46 +0530

    Add files via upload

---
+++ Arithmetic_operations.py
@@ -0,0 +1,22 @@
+'''
+Author:Divya Kuriakose
+Date:08-10-2024
+Program:Basic arithmetic operations
+Version 1.1
+'''
+
+number1=int(input("Enter the first number"))
+number2=int(input("Enter the second number"))
+number3=int(input("Enter the third number"))
+sum=number1+number2
+print("The sum of number1 and number2 is:",sum)
+difference=number2-number1
+print("The difference when number2 is subtracted from number1 is:",difference)
+product=number1*number2
+print("The product of number1 and number2 is:",product)
+quotient=number1/number2
+print("The quotient when number1 is divided by nmber2 is:",quotient)
+remainder=number1%number2
+print("The remainder when number1 is divided by number2 is:",remainder)
+result=(number1+number2)*number3/2
+print("The result of (number1+number2)*number3/2 is:",result)
