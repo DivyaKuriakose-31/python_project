cd92dca0f7b3ab441b84d71cb1c18e41f0ad53cb
DivyaKuriakose-31 <divyakuriakose2028@cs.sjcetpalai.ac.in>
2024-11-20 22:39:20 +0530

    Add files via upload

---
+++ vowels.py
@@ -0,0 +1,12 @@
+'''
+Author:Divya Kuriakose
+Date:29-10-2024
+'''
+
+string_input=input("Enter a string")
+vowels="aeiouAEIOU"
+vowels_count=0
+for char in string_input:
+    if char in vowels:
+        vowels_count+=1
+print(f"The number of given string{string_input}={vowels_count}")
