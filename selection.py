490b9e4b159776a7703d5022b39aba11ee38ea3d
DivyaKuriakose-31 <divyakuriakose2028@cs.sjcetpalai.ac.in>
2024-11-20 23:10:42 +0530

    Add files via upload

---
+++ selection.py
@@ -0,0 +1,11 @@
+'''
+Author:Divya Kuriakose
+Date:15-10-2024
+Python program to check whether the given number is positive or not
+'''
+
+number=int(input("Enter a number:"))
+if number>0:
+    print("The given number:",number," is positive")
+else:
+     print("The given number:",number,"is negative")
