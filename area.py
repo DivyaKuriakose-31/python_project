93d129442b1be9c0795b83dc1d9fcec1293dcf95
DivyaKuriakose-31 <divyakuriakose2028@cs.sjcetpalai.ac.in>
2024-11-20 23:13:13 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,10 @@
+'''
+Author:Divya Kuriakose
+Date: 11-10-2024
+program to find the area of circle
+'''
+
+from math import pi
+radius=float(input("Enter the radius of the circle: "))
+area=pi *radius*radius
+print("The area of the circle is:",area)
