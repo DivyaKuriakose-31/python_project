7bd96125c3486b198577dbced1816f145a8122e5
DivyaKuriakose-31 <divyakuriakose2028@cs.sjcetpalai.ac.in>
2024-11-20 23:26:56 +0530

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,12 @@
+'''
+Author :Divya Kuriakose
+Date:11-10-2024
+'''
+
+num1=10
+num2=5
+print("Sum:",num1+num2,",""Division:",num1/num2)
+print("Is num1 is greater than num2?",num1>num2)
+print("Are num1 and num2 equal?",num1==num2)
+print("Logical AND: ",num1>num2 & num2<num1)
+print("Logical OR: ",num1>num2 | num2<num1)
