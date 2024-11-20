03ab4bf86c98e7b74ad8b91733370e278e380a51
DivyaKuriakose-31 <divyakuriakose2028@cs.sjcetpalai.ac.in>
2024-11-20 23:08:29 +0530

    Add files via upload

---
+++ greater.py
@@ -0,0 +1,14 @@
+'''
+Author:Divya Kuriakose
+Date:15-10-2024
+Program to find which number is greater
+'''
+
+num1=input("Enter first number:")
+num2=input("Enter second number:")
+if(num1>num2):
+    print(num1,"is greater than",num2)
+elif(num1<num2):
+    print(num2,"is greater than",num1)
+else:
+    print("The two numbers are equal")
