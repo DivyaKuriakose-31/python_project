3c186f04731cbd33446a0b621ad904cf49a2297a
DivyaKuriakose-31 <divyakuriakose2028@cs.sjcetpalai.ac.in>
2024-11-20 23:07:12 +0530

    Add files via upload

---
+++ eligibility.py
@@ -0,0 +1,11 @@
+'''
+Author:Divya Kuriakose
+Date:15-10-2024
+Program to check whether the person is eligible for driving license
+'''
+
+age=int(input("Enter your age:"))
+if(age>=18):
+    print("You are eligible for driving license")
+else:
+    print("You are not eligible for driving license")
