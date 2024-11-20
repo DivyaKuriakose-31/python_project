bd931cdf812bcc0eec25fbd0ca78d9a39c005bc9
DivyaKuriakose-31 <divyakuriakose2028@cs.sjcetpalai.ac.in>
2024-11-20 23:26:38 +0530

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,10 @@
+'''
+Author :Divya Kuriakose
+Date:11-10-2024
+'''
+
+import math
+number=int(input("Enter a number: "))
+print("square root of",number,":",math.sqrt(number))
+print("factorial of",number,":",math.factorial(number))
+print(number,"Raised to power 2",number,":",math.pow(number,2))
