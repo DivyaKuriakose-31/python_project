45a9ac74972adcf6c72bd68fde8d9b1b4c3a727f
DivyaKuriakose-31 <divyakuriakose2028@cs.sjcetpalai.ac.in>
2024-11-20 23:09:36 +0530

    Add files via upload

---
+++ person_type.py
@@ -0,0 +1,11 @@
+'''
+Author:Divya Kuriakose
+Date:15-10-2024
+Program to find whether the person is major or minor
+'''
+
+age=int(input("Enter your age:"))
+if(age>=18):
+    print("You are major")
+else:
+    print("You are minor")
