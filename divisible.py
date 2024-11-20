1a280b442318d5d0e787c62cd6a3680f195d0169
DivyaKuriakose-31 <divyakuriakose2028@cs.sjcetpalai.ac.in>
2024-11-20 23:05:53 +0530

    Add files via upload

---
+++ divisible.py
@@ -0,0 +1,11 @@
+'''
+Author:Divya Kuriakose
+Date:15-10-2024
+Program to check whether the number is divisible by five
+'''
+
+number=int(input("Enter a number"))
+if(number%5==0):
+   print("Divisible by 5")
+else:
+    print("Not divisible by 5")
