127fe8d3525ffff7d1531f80435799542940eff6
DivyaKuriakose-31 <divyakuriakose2028@cs.sjcetpalai.ac.in>
2024-11-20 23:22:28 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,10 @@
+'''
+Author :Divya Kuriakose
+Date:11-10-2024
+'''
+
+name=input("Enter your name: ")
+age=(input("Enter your age: "))
+print("Enter your name: ",name)
+print("Enter your age: ",age)
+print("Hello,"+" "+name+"!"+" "+"You are"+" "+age+" "+"years old")
