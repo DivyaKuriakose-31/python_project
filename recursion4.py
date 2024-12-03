a91bad2a8d38a74f4358edd5dba18ae1b486a1f8
DivyaKuriakose-31 <divyakuriakose2028@cs.sjcetpalai.ac.in>
2024-12-03 10:47:20 +0530

    Add files via upload

---
+++ recursion4.py
@@ -0,0 +1,15 @@
+'''
+Author :Divya Kuriakose
+Date:3-12-2024
+Program to find the GCD using recursion
+'''
+
+def gcd(num1,num2):
+    if num1%num2 ==0:
+        return num2
+    else:
+        return gcd(num2,num1%num2)
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+gcd(num1,num2)
+print(f"The gcd of given numbers are:{gcd(num1, num2)}")
